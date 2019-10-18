966aeb437443a947e29856ed2d4feaf70b75f015
tomaswww <twingord@gmail.com>
2019-10-18 18:30:41 -0300

    Updated environment data

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'tw-auth.auth0.com', // the auth0 domain prefix
+    audience: 'menu', // the audience set for the auth0 app
+    clientId: 'blQRgkRGWHn9dePAcFBOnD2JZSV1TLjO', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
