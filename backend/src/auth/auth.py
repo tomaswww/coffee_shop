02370073af63bb5edb5517c3f63856d90913a74a
tomaswww <twingord@gmail.com>
2019-10-18 01:04:01 -0300

    Added auth0 domain

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -5,7 +5,7 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
+AUTH0_DOMAIN = 'tw-auth.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'dev'
 
