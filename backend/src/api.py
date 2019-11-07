298997e1be331fa43fec690a8f79cc79d56e0990
tomaswww <twingord@gmail.com>
2019-11-07 01:58:11 -0300

    Fixed return variable for drinks endpoint

--- backend/src/api.py
+++ backend/src/api.py
@@ -4,7 +4,8 @@ from sqlalchemy import exc
 import json
 from flask_cors import CORS
 
-from .database.models import db_drop_and_create_all, setup_db, Drink
+
+from .database.models import db_drop_and_create_all,setup_db,Drink
 from .auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
@@ -17,6 +18,7 @@ CORS(app)
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
 db_drop_and_create_all()
+
 
 # ROUTES
 '''
@@ -37,7 +39,7 @@ def get_drinks():
     if not drinks:
         abort(404)
     return jsonify({
-        "success": True, "drinks": "drinks"
+        "success": True, "drinks": drinks
     })
 
 
